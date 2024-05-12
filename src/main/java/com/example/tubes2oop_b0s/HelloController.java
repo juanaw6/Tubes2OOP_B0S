bfd12ea85d5541aee5891715c4986cbbe1cae08a
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-05-13 04:43:23 +0700

    init repo

---
+++ src/main/java/com/example/tubes2oop_b0s/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.tubes2oop_b0s;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
