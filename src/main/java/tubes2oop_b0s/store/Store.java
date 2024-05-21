5e00c5f5afca303b369df6d705c3bc6b5530fe18
FarelW <eeksapi321@gmail.com>
2024-05-22 01:46:40 +0700

    feat: initiate store

--- src/main/java/tubes2oop_b0s/store/Store.java
+++ src/main/java/tubes2oop_b0s/store/Store.java
@@ -1,16 +1,17 @@
 package tubes2oop_b0s.store;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import tubes2oop_b0s.card.Card;
 
 public class Store {
     private static Store instance;
-    private ArrayList<Card> products;
+    private Map<Card, Integer> products;
 
     private Store() {
         // Initialize the store data
-        products = new ArrayList<Card>(100);
+        products = new HashMap<>();
     }
 
     public static Store getInstance() {
@@ -22,5 +23,22 @@ public class Store {
 
     public void displayProducts() {
         // Method to display products in Store
+        for (Map.Entry<Card, Integer> entry : products.entrySet()) {
+            System.out.println(entry.getKey().getName() + ": " + entry.getValue());
+        }
+    }
+
+    public void handleBuy(Card card, int quantity) {
+        if (products.containsKey(card) && products.get(card) >= quantity) {
+            products.put(card, products.get(card) - quantity);
+            System.out.println("Membeli " + quantity + " " + card.getName()+" dari toko");
+        } else {
+            System.out.println("Jumlah " + card.getName() + " di toko tidak cukup");
+        }
+    }
+
+    public void handleSell(Card card, int quantity) {
+        products.put(card, products.getOrDefault(card, 0) + quantity);
+        System.out.println("Menjual " + quantity + " " + card.getName());
     }
 }
